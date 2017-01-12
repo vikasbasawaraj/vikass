e73a63c871a0b5a9bd0bfaa0e27ee060a7f009da
vikasbasawaraj <shivapujevikas@gmail.com>
2017-01-12 13:19:16 +0530

    modified mainactivity added new file testaddfile.java

--- app/src/main/java/com/example/vankudre/gittest/MainActivity.java
+++ app/src/main/java/com/example/vankudre/gittest/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.vankudre.gittest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+private  void TestUpLoad(){
+   int i=1+1;
+    int ii=2+2;
+    int iii=3+3;
+}
+
+
+
 }
---
+++ app/src/main/java/com/example/vankudre/gittest/testaddfile.java
@@ -0,0 +1,8 @@
+package com.example.vankudre.gittest;
+
+/**
+ * Created by VANKUDRE on 12-01-2017.
+ */
+
+public class testaddfile {
+}
